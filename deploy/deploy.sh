2eb89c767cbc90dd9482c92ab09a34c54f9cbe5d
Ryan Tsao <ryan.j.tsao@gmail.com>
2019-05-03 11:43:59 -0700

    Fix team

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-APP_URL=$(now --npm -t $NOW_TOKEN --public -e PRIVATE_KEY=$PRIVATE_KEY -e APP_ID=$APP_ID -e WEBHOOK_SECRET=$WEBHOOK_SECRET -e NODE_ENV="production")
+APP_URL=$(now --npm -T $NOW_TEAM -t $NOW_TOKEN --public -e PRIVATE_KEY=$PRIVATE_KEY -e APP_ID=$APP_ID -e WEBHOOK_SECRET=$WEBHOOK_SECRET -e NODE_ENV="production")
 now scale $APP_URL sfo 1 -T $NOW_TEAM --token=$NOW_TOKEN
 now alias set $APP_URL $NOW_ALIAS -t $NOW_TOKEN
 now rm $NOW_PROJECT --safe -T $NOW_TEAM -t $NOW_TOKEN -y || true
